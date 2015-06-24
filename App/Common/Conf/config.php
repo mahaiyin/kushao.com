83ed77c68a68aa48423bad62cea81366173f802e
mahaiyin <mahaiyin@qq.com>
2015-06-24 19:03:56 +0800

    1

---
+++ App/Common/Conf/config.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+	
+	//分组列表
+	'MODULE_ALLOW_LIST' => array(
+		'Home',
+	),
+	
+	//默认分组
+	'DEFAULT_MODULE' => 'Home',
+	
+	//路由模式
+	'URL_MODEL' => 2,
+	
+	//模版标签
+	'TMPL_L_DELIM' => '<{',
+	'TMPL_R_DELIM' => '}>',
+
+	'URL_ROUTER_ON' => true,
+	'URL_ROUTE_RULES' => array(
+		array('a','Home/Index/a'),
+	),
+);
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+const APP_DEBUG = true;//开启调试模式
+const DIR_SECURE_FILENAME = false;//不生成安全文件
+const APP_PATH = './App/';//定义应用目录
+require './ThinkPHP/ThinkPHP.php';//引入ThinkPHP文件
